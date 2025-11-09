f211e5741d04609b149ddc92848c7a3d26c898fc
Arun <arunlrajamanickam.com>
2025-11-09 15:13:22 +0530

    SO schema

--- fmcg_cp/hooks.py
+++ fmcg_cp/hooks.py
@@ -10,6 +10,11 @@ app_license = "mit"
 
 # required_apps = []
 
+fixtures = [
+    {"dt": "Client Script", "filters": [["module", "=", "FMCG-CP"]]}
+]
+
+
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
 # 	{
