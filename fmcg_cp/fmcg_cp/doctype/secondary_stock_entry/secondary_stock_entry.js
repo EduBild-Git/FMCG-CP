228ef2efb8bc8680c5967fac3a88ab48001d00a8
daiyan52 <rehanalamkkr@gmail.com>
2025-12-09 13:24:43 +0530

    updating cp module

---
+++ fmcg_cp/fmcg_cp/doctype/secondary_stock_entry/secondary_stock_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Edubild and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Secondary Stock Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });
